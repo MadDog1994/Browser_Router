31f2fff41e1d953be9e1450bcf23d335dd3d6e26
Ostap <flaminis000111@gmail.com>
2022-01-24 20:57:25 +0200

    update info

--- src/components/navigation/photo/DataPhoto.js
+++ src/components/navigation/photo/DataPhoto.js
@@ -4,10 +4,10 @@ import "./DataPhoto.css";
 
 const Photo = () => {
     const url = [
-        "https://varianty.lviv.ua/thumbnails/320x180/1b532115b2d7d52cac401f4c8b960102.jpg",
-        "https://i.imgur.com/o9IJ7e6.jpeg",
-        "https://scontent.fiev22-1.fna.fbcdn.net/v/t31.18172-8/10362822_1502883493279214_8164876466224249799_o.jpg?_nc_cat=100&ccb=1-5&_nc_sid=9267fe&_nc_ohc=QZ3Ot5r4AwMAX_QpScW&_nc_ht=scontent.fiev22-1.fna&oh=ea6ee9936a5787abc066fb8442d801af&oe=61D40E3F",
-        "https://www.etoretro.ru/data/media/29/15124730068d8.jpg"
+        "https://root-nation.com/wp-content/uploads/2020/12/1581997813_black-hole_vsthemes_ru-37.jpg",
+        "https://thumbor.forbes.com/thumbor/711x532/https://blogs-images.forbes.com/startswithabang/files/2017/05/sagittarius-a-black-hole-milky-way-center-1200x900.jpg?width=960",
+        "https://interes-fakti.com/wp-content/uploads/2019/09/fakti-o-chernyh-dyrah.jpg",
+        "https://cs8.pikabu.ru/post_img/big/2019/01/22/5/1548139738192673201.jpg"
     ]
 
     return (
--- src/components/navigation/post/DataPost.js
+++ src/components/navigation/post/DataPost.js
@@ -5,6 +5,8 @@ import "./DataPost.css"
 const DataPost = () => {
     const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
     const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+    const LEIA_IMAGE = "https://www.vokrug.tv/pic/news/5/d/1/a/rsz300x300_5d1a2686601497edc7110cdec68d62e9.jpg";
+    const DART_IMAGE = "https://www.peoples.ru/character/movie/darth_vader/darth_1.jpeg";
     return (
         <div className="wrapper">
         <Post
@@ -21,12 +23,12 @@ const DataPost = () => {
             <br/>
             <Post
                 author={{
-                    name: "Anakin skywalker",
-                    photo: ANAKIN_IMAGE,
-                    nickname: "@dart_vader"
+                    name: "Princess Leia",
+                    photo: LEIA_IMAGE,
+                    nickname: "@Leia"
                 }}
-                content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
-                image={RAY_IMAGE}
+                content="The dark side is waiting for everyone"
+                image={DART_IMAGE}
                 date={"26 февр."}
 
             />
